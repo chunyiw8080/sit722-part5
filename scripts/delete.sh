46e142a24a8c5f4f3570551aa2ba364610bd681b
root <root@LAPTOP-SIMCHMEP>
2024-09-13 15:14:41 +1000

    update delete microservice scripts

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,1 +1,8 @@
-# Your Solution
+set -u
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+: "$DB_URL"
+
+envsubst < ./scripts/kubernetes/deployment.yaml | kubectl delete -f -
+envsubst < ./scripts/kubernetes/service.yaml | kubectl delete -f -
+envsubst < ./scripts/kubernetes/configmap.yaml | kubectl delete -f -
