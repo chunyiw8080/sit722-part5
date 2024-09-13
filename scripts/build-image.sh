3194472220536bdaf079d0264187b6d1e188f9e5
root <root@LAPTOP-SIMCHMEP>
2024-09-13 14:21:58 +1000

    update build image script

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -4,8 +4,6 @@ set -u
 : $CONTAINER_REGISTRY
 : $VERSION
 
-docker build -t $CONTAINER_REGISTRY/book-catalog:$VERSION --file ./book_catalog/Dockerfile . > /dev/null 2>&1
-echo "Book-catalog image build complete"
+docker build -t $CONTAINER_REGISTRY/book-catalog:$VERSION --file ./book_catalog/Dockerfile .
 
-docker build -t $CONTAINER_REGISTRY/inventory:$VERSION --file ./inventory_management/Dockerfile . > /dev/null 2>&1
-echo "Inventory_management image build complete"
+docker build -t $CONTAINER_REGISTRY/inventory:$VERSION --file ./inventory_management/Dockerfile .
