96fc9070dd740861b8ec6641fe8ad25f0b88ac42
root <root@LAPTOP-SIMCHMEP>
2024-09-20 13:13:52 +1000

    update terrform action files

---
+++ scripts/terraform/kubernetes.tf
@@ -0,0 +1,25 @@
+resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
+  name = var.app_name
+  location = var.location
+  resource_group_name = azurerm_resource_group.deakinuni.name
+  dns_prefix = var.app_name
+  kubernetes_version = var.kubernetes_version
+
+  default_node_pool {
+    name = "default"
+    node_count = 1
+    vm_size = "standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "role_assignment" {
+  #Allow kubelet component on kubernetes cluster to pull image from container registry
+  principal_id = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
+  scope = azurerm_container_registry.container_registry.id
+  role_definition_name = "AcrPull"
+  skip_service_principal_aad_check = true
+}
---
+++ scripts/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "deakinuni"
+}
+
+variable "location" {
+  default = "australiaeast"
+}
+
+variable "app_name" {
+    default = "task92c"
+}
+
+variable "kubernetes_version" {
+    default = "1.30.3"
+}
